7fe209fa60a768232062d9ffdb3cfe07284be53a
Wilson <127255876+MaldoWilson@users.noreply.github.com>
2023-12-07 17:20:25 -0300

    commit
    bluetooth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { Router, RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
+// Angular Firebase
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+// Otras importaciones
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { environment } from 'src/environments/environment';
 import { NgApexchartsModule } from 'ng-apexcharts';
 import { ConsumoMensualPageModule } from './vistas/consumo-mensual/consumo-mensual.module';
-import { ConsumoMensualPage } from './vistas/consumo-mensual/consumo-mensual.page';
+import { BluetoothSerial } from '@awesome-cordova-plugins/bluetooth-serial/ngx';
 
 @NgModule({
   declarations: [
@@ -27,9 +30,12 @@ import { ConsumoMensualPage } from './vistas/consumo-mensual/consumo-mensual.pag
     AngularFirestoreModule,
     NgApexchartsModule,
     ConsumoMensualPageModule,
-
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    BluetoothSerial,
+    // Otros proveedores
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/bluetooth/bluetooth.page.ts
+++ src/app/bluetooth/bluetooth.page.ts
@@ -1,29 +1,84 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { BluetoothSerial } from '@awesome-cordova-plugins/bluetooth-serial/ngx';
+import { Subscription } from 'rxjs';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-bluetooth',
   templateUrl: './bluetooth.page.html',
   styleUrls: ['./bluetooth.page.scss'],
 })
-export class BluetoothPage implements OnInit {
+export class BluetoothPage implements OnInit, OnDestroy {
 
-    receivedData: string[] = [];
+  receivedData: string[] = [];
+  bluetoothSubscription: Subscription;
 
-    constructor(private bluetoothSerial: BluetoothSerial) {}
-  
-    ngOnInit() {
-      this.bluetoothSerial.subscribe('\n').subscribe(
-        (data) => {
-          this.receivedData.push(data);
-        },
-        (error) => {
-          console.error('Error al recibir datos por Bluetooth:', error);
-        }
-      );
-    }
+  constructor(
+    private bluetoothSerial: BluetoothSerial,
+    private alertController: AlertController
+  ) {}
+
+  ngOnInit() {
+    // Buscar dispositivos Bluetooth cercanos
+    this.bluetoothSerial.list().then(devices => {
+      // Muestra la lista de dispositivos disponibles y permite al usuario seleccionar uno
+      this.showDeviceList(devices);
+    }).catch(error => {
+      console.error('Error al buscar dispositivos:', error);
+    });
   }
 
+  ngOnDestroy() {
+    // Detener la suscripción al Bluetooth cuando la página se destruye
+    this.bluetoothSubscription.unsubscribe();
+  }
 
+  private showDeviceList(devices: any[]) {
+    const buttons = devices.map(device => ({
+      text: device.name,
+      handler: () => {
+        // Conectar al dispositivo seleccionado
+        this.connectToDevice(device);
+      }
+    }));
 
+    // Muestra un cuadro de diálogo para seleccionar el dispositivo
+    this.alertController.create({
+      header: 'Selecciona un dispositivo',
+      buttons: [...buttons, 'Cancelar']
+    }).then(alert => {
+      alert.present();
+    });
+  }
 
+  private connectToDevice(device: any) {
+    // Conectar al dispositivo Bluetooth
+    this.bluetoothSerial.connect(device.address).subscribe(
+      success => {
+        console.log('Conexión exitosa:', success);
+
+        // Enviar datos al Arduino
+        this.bluetoothSerial.write('Hello from Ionic!').then(writeSuccess => {
+          console.log('Datos enviados con éxito:', writeSuccess);
+        }).catch(writeError => {
+          console.error('Error al enviar datos:', writeError);
+        });
+
+        // Recibir datos del Arduino
+        this.bluetoothSubscription = this.bluetoothSerial.subscribe('\n').subscribe(
+          data => {
+            console.log('Datos recibidos desde Arduino:', data);
+            this.receivedData.push(data);
+          },
+          receiveError => {
+            console.error('Error al recibir datos:', receiveError);
+          }
+        );
+
+      },
+      connectError => {
+        console.error('Error al conectar al dispositivo:', connectError);
+      }
+    );
+  }
+}
